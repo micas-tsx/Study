16cb9f3229b38b79a196f8ac9cce9a0e02e53635
micas <micaelabud21@gmail.com>
2024-05-10 18:07:16 -0300

    exercio

---
+++ B7/curso js/exercicios/ex2.js
@@ -0,0 +1,36 @@
+// substitua o @ pela condicional correta para que o restultado seja TRUE
+// let x = 10
+// let y = 5
+// console.log(x @ y);
+
+let x = 10
+let y = 5
+console.log(x > y);
+
+// substitua o @ pela condicional correta para que o restultado seja FALSE
+// let w = "10"
+// let z = 10
+// console.log(w @ z);
+
+let w = "10"
+let z = 10
+console.log(w === z)
+
+// substitua o @ pela condicional correta para que o restultado seja TRUE
+// let a = 10
+// let b = 5
+// console.log(x @ y);
+
+let a = 10
+let b = 5
+console.log(a > b)
+
+// Crie uma condicional para verificar se o preço da carne está barato ou caro. PS: até 45 está barato
+
+let preco = 40.3
+
+if (preco <= 45) {
+    console.log('ta barto de mais zé')
+} else {
+    console.log('ta caro de mais zé')
+}
