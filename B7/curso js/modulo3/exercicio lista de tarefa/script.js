bc18a1c3ef6428469e4f47cf5c60720599da5cdc
micas <micaelabud21@gmail.com>
2024-05-24 19:22:37 -0300

    lista de tarefas simples e finalização do modulo 3

---
+++ B7/curso js/modulo3/exercicio lista de tarefa/script.js
@@ -0,0 +1,22 @@
+//  pegando os elementos
+const input = document.querySelector('input')
+const lista = document.querySelector('ul')
+
+//  enventos
+input.addEventListener('keyup', handleKeyUp)
+
+function handleKeyUp(e) {
+    // faz um evento capturando o enter verificando se ele foi acionado, se for
+    if(e.key === 'Enter') {
+        // crai um elemento no novo (no caso um li)
+        const newLi = document.createElement('li')
+        // coloca esse elemento novo com o valor do input
+        newLi.innerHTML = input.value
+        // coloca ele como "filho" do ul, chamado de lista aqui
+        lista.appendChild(newLi)
+
+
+        // zera o campo
+        input.value = ''
+    }
+}
