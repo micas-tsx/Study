bece6486bd922055b825e573c589a612aab27df2
micas <micaelabud21@gmail.com>
2024-05-30 17:10:48 -0300

    inicio do modulo 5

---
+++ B7/curso js/modulo5/req e resp.js
@@ -0,0 +1,15 @@
+/*
+Requisição e Resposta
+
+- Requisição
+Tudo que é acessado na internet, chamada de algo
+    -> Cabeçalhos / Headers
+    Passa as informações(o id, o navegador, cookies), todas as informações necessarias para q o navegador
+    -> Body / Corpo
+    Conteudo bruto passado 
+
+- Resposta
+    -> Também recebe cabeçalhos e body
+    -> passam o HTML ta página por exemplo
+
+*/
---
+++ B7/curso js/modulo5/sincrono e assincrono.js
@@ -0,0 +1,9 @@
+/*
+O que é Sincrono e Assincrona
+
+- Sincrono
+Roda o código linha por linha, um por um
+
+- Assincrono
+Roda tudo de uma vez só logo no inicio
+*/
