cab8693481bacfef41a56cce8b17a13711b43baa
micas-tsx <micaelabud21@gmail.com>
2024-06-05 18:04:15 -0300

    math

---
+++ B7/curso js/modulo 6/matematica.js
@@ -0,0 +1,11 @@
+let novoValor = 
+
+//Math.floor(Math.random() * 100) -> pega um numero aleatorio entre 0 e 1 e multipilca por 100, dps arredonda pra baixo
+//.random() -> reorna um numero aleatorio  entre 0 e 1,tbm usado em conjunto com o .floor
+//.min() / .max() -> retorna o menor e o maior numero de um conjnunto
+//.abs(-3.981) -> retorna o numero absoluto de algum numero, vulgo transforma negativo em positivo 
+//.ceil(4.2) -> arrendonda pra cima 
+//.floor(4.6) -> arredonda sempre pra baixo
+//.round(5.42) -> arredonda o numero, dependendo do valor, tanto pra cima quanto pra baixo
+
+console.log(novoValor)
