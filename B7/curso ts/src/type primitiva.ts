f0cad7545c2bec1dcdc4f8e867ad225febd352d8
Micael Abud <84108378+micas-tsx@users.noreply.github.com>
2024-07-15 11:02:41 -0300

    tipagem primitiva

--- B7/curso ts/src/type primitiva.ts
+++ B7/curso ts/src/type primitiva.ts
@@ -1,1 +1,8 @@
+// essas são as tipagem mais normais de se ver
+// n usar a tipagem sempre, as vezes n é necessario
+
 let nome: string = 'micas' 
+
+let idade: number = 90
+
+let programdor: boolean = true
